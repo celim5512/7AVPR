628dd35a8368bbc1815ce35277fcb5c3d5c2a873
celim5512 <78375907+celim5512@users.noreply.github.com>
2021-05-10 19:03:52 +0300

    bird name time click mouse onclick background
    bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background bird name time click mouse onclick background

---
+++ app.js
@@ -0,0 +1,8 @@
+var b = document.querySelector("button")
+let img = document.querySelector(".bu")
+b.onclick = function(){
+    var angle = 720 + Math.random()*1000
+    var time = 3 + Math.random()*5
+    img.style.transition = `${time}s`
+    img.style.transform = `rotate(${angle}deg)`
+}
